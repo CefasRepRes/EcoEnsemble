10b849b4fd028782574752801eaf33c47e8deadd
JamesMartindaleCefas <james.martindale@cefas.co.uk>
2022-10-01 12:54:46 +0100

    Reworked matrix arrays for compatibilty with Linux

--- src/KF_back.cpp
+++ src/KF_back.cpp
@@ -9,11 +9,16 @@ Eigen::MatrixXd KalmanFilter_back(Eigen::VectorXd rhos, Eigen::VectorXd dee, Eig
 
   Eigen::MatrixXd xhat_b = Eigen::MatrixXd::Zero(Time, xhat.size());
   Eigen::MatrixXd P_ = P;
-  Eigen::MatrixXd P_s[Time];
+  // DO NOT CHANGE THE BELOW. For linux distributions, the below is not a valid definition
+  //  because Time is not know at compile time. To have dynamically sized arrays we must use std::vector.
+  //  This is not a problem for windows distributions, but will cause problems when you submit to CRAN.
+  //Eigen::MatrixXd P_s[Time];
+  //Eigen::MatrixXd G[Time];
+  std::vector<Eigen::MatrixXd> P_s(Time, Eigen::MatrixXd(xhat.size(), y.cols()));
+  std::vector<Eigen::MatrixXd> G(Time, Eigen::MatrixXd(xhat.size(), y.cols()));
   Eigen::MatrixXd A = rhos * rhos.transpose();
   Eigen::VectorXd xhat_ = xhat;
   Eigen::MatrixXd xhat_s = Eigen::MatrixXd::Zero(Time, xhat.size());
-  Eigen::MatrixXd G[Time];
   Eigen::MatrixXd G_ = Eigen::MatrixXd::Zero(xhat.size(), y.cols());
   Eigen::MatrixXd er = Eigen::MatrixXd::Zero(Time, y.cols());
   double est;
@@ -28,11 +33,15 @@ Eigen::MatrixXd KalmanFilter_back(Eigen::VectorXd rhos, Eigen::VectorXd dee, Eig
   for (int i = 0; i < Time; ++i)
   {
     //JM: Need to initialise some of the other variables
-    G[i] = Eigen::MatrixXd(xhat.size(), y.cols());
+    //G[i] = Eigen::MatrixXd(xhat.size(), y.cols());
+    //G.push_back(Eigen::MatrixXd(xhat.size(), y.cols()));
 
-    P_ = P_.cwiseProduct(A) + Q; // l 455
-    xhat_ = rhos.cwiseProduct(xhat_); // l 454
-    P_s[i] = P_ ;
+    P_ = P_.cwiseProduct(A) + Q;
+    xhat_ = rhos.cwiseProduct(xhat_);
+
+    //P_s[i] = P_ ;
+    P_s.at(i) = P_;
+
     xhat_s.row(i) = xhat_.transpose() ;
     for (int j = 0; j < y.cols(); ++j) // change this to some input that lists the numbers of the elements of y that we are interested in, maybe another value that says how many we are interested in.
     {
