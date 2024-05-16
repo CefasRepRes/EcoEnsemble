ae227a91a9ce3366f68dfb3aa7cb446eddb7c7a4
Michael Thomson (Cefas) <michael.thomson@cefas.gov.uk>
2024-05-16 12:52:28 +0100

    Added get_stanmodels test

---
+++ tests/testthat/test-get_stanmodel.R
@@ -0,0 +1,10 @@
+test_that("get ensemble model works", {
+
+  priors_hier <- EnsemblePrior(4)
+  priors_lkj <- EnsemblePrior(4, ind_st_params = IndSTPrior("lkj", list(3, 2), 3, AR_params = c(2,4)))
+
+  expect_equal(get_mcmc_ensemble_model(priors_hier)@model_name, "ensemble_model_hierarchical")
+  expect_equal(get_mcmc_ensemble_model(priors_hier, likelihood = FALSE)@model_name, "ensemble_prior_hierarchical")
+  expect_equal(get_mcmc_ensemble_model(priors_lkj)@model_name, "ensemble_model")
+  expect_equal(get_mcmc_ensemble_model(priors_lkj, likelihood = FALSE)@model_name, "ensemble_prior")
+})
